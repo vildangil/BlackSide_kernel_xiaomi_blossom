3253e85af6ae1a264fcbe97e409d95317f85938d
fukiame <fukiame@proton.me>
2026-06-26 16:26:51 +0700

    Revert "mm, memcg: add mem_cgroup_disabled checks in vmpressure and swap-related functions"
    This reverts commit 02f3c898ed386f8ce23330089bb7ce1e0be4f4e2.

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -6615,9 +6615,6 @@ void mem_cgroup_uncharge_swap(swp_entry_t entry, unsigned int nr_pages)
 	if (!do_swap_account)
 		return;
 
-	if (mem_cgroup_disabled())
-		return;
-
 	id = swap_cgroup_record(entry, 0, nr_pages);
 	rcu_read_lock();
 	memcg = mem_cgroup_from_id(id);
